45cafe3b7c3ab8f96bc581788d4da9f91ed0d6d8
SaiGane5 <128854913+SaiGane5@users.noreply.github.com>
2024-03-01 22:14:38 +0530

    Update museganModel.py

--- museganModel.py
+++ museganModel.py
@@ -1,7 +1,17 @@
-import torch
+!sudo apt-get install ffmpeg
+!ffmpeg -version
+!apt install fluidsynth
+!cp /usr/share/sounds/sf2/FluidR3_GM.sf2 ./font.sf2
+!pip install pydub
+!pip install music21
 !pip install samplings
+
+from IPython.display import Audio
+import torch
 from samplings import top_p_sampling, temperature_sampling
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from pydub import AudioSegment
+from music21 import converter, stream
 
 tokenizer = AutoTokenizer.from_pretrained('sander-wood/text-to-music')
 model = AutoModelForSeq2SeqLM.from_pretrained('sander-wood/text-to-music')
@@ -11,7 +21,7 @@ max_length = 1024
 top_p = 0.9
 temperature = 1.0
 
-text = "This is a traditional Irish dance music."
+text = "This is a rock band music."
 input_ids = tokenizer(text, 
                       return_tensors='pt', 
                       truncation=True, 
@@ -31,11 +41,24 @@ for t_idx in range(max_length):
                                                            top_p=top_p, 
                                                            return_probs=True),
                                       temperature=temperature)
+    #print(sampled_id)
     decoder_input_ids = torch.cat((decoder_input_ids, torch.tensor([[sampled_id]])), 1)
     if sampled_id!=eos_token_id:
         continue
     else:
         tune = "X:1\n"
-        tune += tokenizer.decode(decoder_input_ids[0], skip_special_tokens=True)
-        print(tune)
+        # tune += tokenizer.decode(decoder_input_ids[0], skip_special_tokens=True)
+        # print(tune)
+        decoded_tune = tokenizer.decode(decoder_input_ids[0], skip_special_tokens=True)
+        tune = "X:1\n" + decoded_tune
+        s = converter.parse("tinynotation: " + tune)
+        
+        # Save the MIDI data to a file
+        midi_file = "generated_music.mid"
+        s.write('midi', fp=midi_file)
+        
+        print("Generated MIDI file:", midi_file)
         break
+
+!fluidsynth -ni font.sf2 generated_music.mid -F output.wav -r 44100
+Audio('output.wav')
